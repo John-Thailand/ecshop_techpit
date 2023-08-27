9a1d067ca485dd13a9eb00c8a7b6cd363aa40e15
John-Thailand <hamoban69@gmail.com>
2023-08-27 16:16:39 +0900

    画面を縦向きに固定する

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:ecshop_techpit/app.dart';
+import 'package:flutter/services.dart';
 
 // main.dart runApp 実行以前に必要な実装の集まり
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const App());
 }
