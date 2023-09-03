11454f303a43d215bf144035f8329ed3ffbbaa07
John-Thailand <hamoban69@gmail.com>
2023-09-03 11:54:25 +0900

    ホーム画面でRiverpodを使う

--- lib/model/data/item/item.dart
+++ lib/model/data/item/item.dart
@@ -1,4 +1,5 @@
 // ignore: unused_import
+import 'package:ecshop_techpit/model/category.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -12,7 +13,7 @@ class Item with _$Item {
     required String name,
     required String imageUrl,
     required String description,
-    required String category,
+    required ItemCategory category,
     required List<String> options,
     required int price,
   }) = _Item;
--- lib/model/shop_item.dart
+++ lib/model/shop_item.dart
@@ -1,13 +1,23 @@
 import 'dart:convert' show jsonDecode;
+import 'package:ecshop_techpit/model/category.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ecshop_techpit/model/data/item/item.dart';
 
-final shopItemIdsProvider = Provider((ref) {
+final shopItemIdsForSelectedCategoryProvider = Provider((ref) {
   final AsyncValue<List<Item>> items = ref.watch(shopItemProvider);
+  final selectedCategory = ref.watch(selectedCategoryProvider);
   // ローディング状態やエラー状態については無視し、単にデータの受け取りを待つという実装で済ませます。
   // この様な場合は whenData を使います。
-  return items.whenData((items) => items.map((item) => item.id).toList());
+  return items.whenData((items) => items
+      .where((item) => item.category == selectedCategory)
+      .map((item) => item.id)
+      .toList());
+});
+
+final shopItemForIdProvider = Provider.family((ref, String id) {
+  final AsyncValue<List<Item>> items = ref.watch(shopItemProvider);
+  return items.whenData((items) => items.firstWhere((item) => item.id == id));
 });
 
 final shopItemProvider = FutureProvider((ref) async {
